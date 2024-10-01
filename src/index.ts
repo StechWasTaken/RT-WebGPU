80f70e628023232989fe92b96fe05292ce3fe36c
StechWasTaken <SPprofile1997@gmail.com>
2024-10-02 01:55:28 +0200

    progress raytracing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import squareShader from "./shaders/square.wgsl";
 import raytracingShader from "./shaders/raytracing.wgsl";
 import square from "./shapes/square";
+import BufferFactory from "./helpers/bufferFactory";
 
 if (!navigator.gpu) {
     throw new Error("WebGPU not supported on this browser.");
@@ -17,8 +18,6 @@ const canvas = document.querySelector("canvas");
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
-const canvasDimensions = new Float32Array([canvasWidth, canvasHeight]);
-
 const device = await adapter.requestDevice();
 const context = canvas.getContext("webgpu");
 const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
@@ -28,9 +27,50 @@ context.configure({
     format: canvasFormat,
 });
 
-const uniformBuffer = device.createBuffer({
+const bindGroupLayout = device.createBindGroupLayout({
+    entries: [
+        {
+            binding: 0,
+            visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT, // Uniform used in both stages
+            buffer: {
+                type: "uniform",
+            },
+        },
+        {
+            binding: 1,
+            visibility: GPUShaderStage.FRAGMENT,
+            buffer: {
+                type: "read-only-storage",
+            }
+        },
+        {
+            binding: 2,
+            visibility: GPUShaderStage.FRAGMENT,
+            buffer: {
+                type: "uniform",
+            }
+        }
+    ],
+});
+
+const canvasDimensions = new Float32Array([canvasWidth, canvasHeight]);
+const time = Date.now().toString();
+const index = Math.floor(time.length / 2);
+const part1 = parseInt(time.slice(0, index));
+const part2 = parseInt(time.slice(index));
+const randomValues = new Float32Array([part1, part2]);
+
+console.log(randomValues);
+
+const canvasUniformBuffer = device.createBuffer({
     label: "canvas dimensions",
     size: canvasDimensions.byteLength,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+
+const randomUniformBuffer = device.createBuffer({
+    label: "RNG",
+    size: randomValues.byteLength,
     usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
 });
 
@@ -40,20 +80,34 @@ spheres.push({
     center: {
         x: 0,
         y: 0,
-        z: 0,
+        z: -1,
     },
-    r: 1,
+    r: 0.5,
 });
 
-device.queue.writeBuffer(uniformBuffer, 0, canvasDimensions);
+spheres.push({
+    center: {
+        x: 0,
+        y: -100.5,
+        z: -1,
+    },
+    r: 100,
+});
+
+const info = BufferFactory.prepareForBuffer(spheres);
+const bufferReadySpheres = new Float32Array(info.data);
+
+const spheresStorageBuffer = device.createBuffer({
+    label: "sphere instances",
+    size: info.offset,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+});
 
 const vertexBuffer = device.createBuffer({
     label: "square vertices",
     size: square.byteLength,
     usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
 });
-
-device.queue.writeBuffer(vertexBuffer, 0, square);
 
 const vertexBufferLayout: GPUVertexBufferLayout = {
     arrayStride: 8,
@@ -64,19 +118,14 @@ const vertexBufferLayout: GPUVertexBufferLayout = {
     }],
 };
 
+device.queue.writeBuffer(spheresStorageBuffer, 0, bufferReadySpheres);
+device.queue.writeBuffer(canvasUniformBuffer, 0, canvasDimensions);
+device.queue.writeBuffer(randomUniformBuffer, 0, randomValues);
+device.queue.writeBuffer(vertexBuffer, 0, square);
+
 const squareShaderModule = device.createShaderModule({
     label: "square shader",
     code: raytracingShader,
-});
-
-const bindGroupLayout = device.createBindGroupLayout({
-    entries: [{
-        binding: 0,
-        visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT, // Uniform used in both stages
-        buffer: {
-            type: "uniform",
-        },
-    }],
 });
 
 const pipelineLayout = device.createPipelineLayout({
@@ -98,24 +147,53 @@ const squareRenderPipeline = device.createRenderPipeline({
             format: canvasFormat,
         }],
     },
+    multisample: {
+        count: 4,
+    },
 });
 
 const bindGroup = device.createBindGroup({
     label: "square renderer bind group",
     layout: squareRenderPipeline.getBindGroupLayout(0),
-    entries: [{
-        binding: 0,
-        resource: { 
-            buffer: uniformBuffer,
+    entries: [
+        {
+            binding: 0,
+            resource: { 
+                buffer: canvasUniformBuffer,
+            },
         },
-    }],
+        {
+            binding: 1,
+            resource: {
+                buffer: spheresStorageBuffer,
+            }
+        },
+        {
+            binding: 2,
+            resource: {
+                buffer: randomUniformBuffer,
+            }
+        }
+    ],
 });
 
 const encoder = device.createCommandEncoder();
 
+const canvasTexture = context.getCurrentTexture();
+
+let multisampleTexture: GPUTexture;
+
+multisampleTexture = device.createTexture({
+    format: canvasTexture.format,
+    usage: GPUTextureUsage.RENDER_ATTACHMENT,
+    size: [canvasTexture.width, canvasTexture.height],
+    sampleCount: 4,
+});
+
 const pass = encoder.beginRenderPass({
     colorAttachments: [{
-        view: context.getCurrentTexture().createView(),
+        view: multisampleTexture.createView(),
+        resolveTarget: canvasTexture.createView(),
         loadOp: "clear",
         storeOp: "store",
     }]
--- src/shaders/raytracing.wgsl
+++ src/shaders/raytracing.wgsl
@@ -1,36 +1,143 @@
+struct Sphere {
+    center: vec3f,
+    r: f32,
+}
+
+struct Ray {
+    origin: vec3f,
+    direction: vec3f,
+}
+
+struct HitRecord {
+    p: vec3f,
+    normal: vec3f,
+    t: f32,
+    frontFace: bool,
+}
+
 @group(0) @binding(0) var<uniform> canvas: vec2f;
+@group(0) @binding(1) var<storage, read> spheres: array<Sphere>;
+@group(0) @binding(2) var<uniform> rng: vec2f;
 
 const CAMERA_CENTER = vec3f(0, 0, 0);
+const MAX_BOUNCES = 100;
 
-fn rayAt(origin: vec3f, rayDirection: vec3f, t: f32) -> vec3f {
-    return origin + t * rayDirection;
+fn random(p: vec2f) -> f32 {
+    let h = dot(p * rng, vec2f(127.1, 311.7));
+    return fract(sin(h) * 43758.5453123);
 }
 
-fn hasHitSphere(center: vec3f, radius: f32, rayOrigin: vec3f, rayDirection: vec3f) -> f32 {
-    let oc = center - rayOrigin;
-    let a = length(rayDirection);
-    let h = dot(rayDirection, oc);
-    let c = length(oc) - radius * radius;
-    let discriminant = h * h - a * c;
-    
-    if (discriminant < 0) {
-        return -1.0;
+fn randomRange(p: vec2f, min: f32, max: f32) -> f32 {
+    return random(p) * (max - min) + min;
+}
+
+fn randomUnitVector(p: vec2f) -> vec3f {
+    var q: vec3f; 
+    var lensq: f32;
+    while (true) {
+        q = randomRange(p, -1, 1) * vec3f(1,1,1);
+        lensq = dot(q, q);
+        if (1.175e-38 < lensq && lensq <= 1) {
+            break;
+        }
+    }
+    return q / sqrt(lensq);
+} 
+
+fn randomOnHemisphere(normal: vec3f, p: vec2f) -> vec3f {
+    let onUnitSphere = randomUnitVector(p);
+    if (dot(onUnitSphere, normal) > 0.0) {
+        return onUnitSphere;
     } else {
-        return (h - sqrt(discriminant)) / a;
+        return -onUnitSphere;
     }
 }
 
-fn rayColor(rayOrigin: vec3f, rayDirection: vec3f) -> vec3f {
-    let t = hasHitSphere(vec3f(0, 0, -1), 0.5, rayOrigin, rayDirection);
-    
-    if (t > 0.0) {
-        let N = normalize(rayAt(rayOrigin, rayDirection, t) - vec3(0, 0, -1));
-        return 0.5 * (N + 1);
+fn setFaceNormal(record: ptr<function, HitRecord>, ray: Ray, outwardNormal: vec3f) {
+    let a = dot(ray.direction, outwardNormal);
+    if (a < 0) {
+        record.frontFace = true;
+        record.normal = outwardNormal;
+    } else {
+        record.frontFace = false;
+        record.normal = -outwardNormal;
+    }
+}
+
+fn at(ray: Ray, t: f32) -> vec3f {
+    return ray.origin + t * ray.direction;
+}
+
+fn hitSpheres(spheres: ptr<storage, array<Sphere>>, ray: Ray, record: ptr<function, HitRecord>, rayTmin: f32, rayTmax: f32) -> bool {
+    let tempRecord: ptr<function, HitRecord> = record;
+    var hitAnything = false;
+    var closestSoFar = rayTmax;
+
+    for (var i: u32 = 0; i < arrayLength(spheres); i++) {
+        let sphere = spheres[i];
+        if (hit(sphere, ray, tempRecord, rayTmin, closestSoFar)) {
+            hitAnything = true;
+            closestSoFar = tempRecord.t;
+            *record = *tempRecord;
+        }
     }
 
-    let unitDirection = normalize(rayDirection);
-    let a = 0.5 * (unitDirection.y + 1.0);
-    return (1.0 - a) * vec3f(1.0, 1.0, 1.0) + a * vec3f(0.5, 0.7, 1.0);
+    return hitAnything;
+}
+
+fn hit(sphere: Sphere, ray: Ray, record: ptr<function, HitRecord>, rayTmin: f32, rayTmax: f32) -> bool {
+    let oc = sphere.center - ray.origin;
+    let a = dot(ray.direction, ray.direction);
+    let h = dot(ray.direction, oc);
+    let c = dot(oc, oc) - sphere.r * sphere.r;
+
+    let discriminant = h * h - a * c;
+    if (discriminant < 0) {
+        return false;
+    }
+    
+    let sqrtd = sqrt(discriminant);
+
+    var root = (h - sqrtd) / a;
+    if (root <= rayTmin || rayTmax <= root) {
+        root = (h + sqrtd) / a;
+        if (root <= rayTmin || rayTmax <= root) {
+            return false;
+        }
+    }
+
+    record.t = root;
+    record.p = at(ray, record.t);
+    record.normal = (record.p - sphere.center) / sphere.r;
+
+    return true;
+}
+
+fn rayColor(spheres: ptr<storage, array<Sphere>>, ray: Ray, p: vec2f) -> vec3f {
+    var color = vec3f(1.0, 1.0, 1.0);
+    var currentRay = ray;
+
+    var i: u32;
+    for (i = 0; i < MAX_BOUNCES; i++) {
+        var record = HitRecord();
+
+        if (hitSpheres(spheres, currentRay, &record, 0, 1e16)) {
+            let direction = record.normal + randomUnitVector(p);
+            currentRay = Ray(record.p, direction);
+            color *= 0.5;
+        } else {
+            let unitDirection = normalize(currentRay.direction);
+            let a = 0.5 * (unitDirection.y + 1.0);
+            color *= (1.0 - a) * vec3f(1.0, 1.0, 1.0) + a * vec3f(0.5, 0.7, 1.0); 
+            break;
+        }
+    }
+
+    if (i == MAX_BOUNCES) {
+        return vec3f(0,0,0);
+    }
+
+    return color;
 }
 
 @vertex
@@ -60,7 +167,9 @@ fn fragmentMain(@builtin(position) pos: vec4f) -> @location(0) vec4f {
     let pixelCenter = pixel00Location + (pos.x * pixelDeltaU) + (pos.y * pixelDeltaV);
     let rayDirection = pixelCenter - CAMERA_CENTER;
 
-    let pixelColor = rayColor(CAMERA_CENTER, rayDirection);
+    let ray = Ray(CAMERA_CENTER, rayDirection);
+
+    let pixelColor = rayColor(&spheres, ray, pos.xy);
 
     return vec4f(pixelColor, 1.0);
 }
